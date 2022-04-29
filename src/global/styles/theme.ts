199dcf180367a4c3654b3520267e5ec211181141
riansco14 <rian.rabelo@ufrpe.br>
2022-04-28 23:38:18 -0300

    Config theme

---
+++ src/global/styles/theme.ts
@@ -0,0 +1,28 @@
+export default {
+    colors: {
+        primary: '#5636d3',
+        purple: '#B22E6F',
+
+        secondary: '#ff872c',
+        secondary_light: 'rgba(255,135,44,0.3)',
+
+        sucess: '#12a454',
+        sucess_light: 'rgba(18,164,84,0.5)',
+
+        attention: '#e83f5b',
+        attention_light: 'rgba(232,63,91,0.5)',
+
+        shape: '#ffffff',
+        title: '#363f5f',
+        text: '#969cb2',
+        text_dark: '#17181f',
+        background: '#f0f2f5',
+
+
+    },
+    fonts: {
+        light: 'Heebo_300Light',
+        regular : 'Heebo_400Regular',
+        medium : 'Heebo_500Medium',
+    },
+};
